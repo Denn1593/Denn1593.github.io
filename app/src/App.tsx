e9a7ea5d320741d8704ddc48df530555114fd801
Denn1593 <oowej@hotmail.com>
2023-03-19 10:03:53 +0100

    updates

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { PrivacyPage } from "./PrivacyPage";
+
+function App() {
+
+  return (
+    <div className="App">
+      <p className="bg-red-600 w-full">{':)'}</p>
+    </div>
+  );
+}
+
+export default App;
